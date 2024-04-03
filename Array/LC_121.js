6832ef846e251ff80480ee15a29faff98454dacc
Vinayak Pandey <109685109+Vinayakpandey286@users.noreply.github.com>
2024-04-04 02:17:51 +0530

    Best time to buy a stock

--- Array/LC_121.js
+++ Array/LC_121.js
@@ -2,19 +2,30 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(prices) {
-    let profit =0
-    let i =1
-    let min= prices[0]
+var maxProfit = function (prices) {
+  let profit = 0;
+  let min = prices[0];
 
-    while(i<prices.length){
-        if(prices[i]-min>profit){
-            profit=prices[i]-min
-        }
-        if(prices[i]<min){
-            min=prices[i]
-        }
-        i++
+  for (let i = 1; i < prices.length; i++) {
+    // can i sell?
+    if (prices[i] > min) {
+      // you can sell but check if it better profit than previous one
+      profit = Math.max(profit, prices[i] - min);
     }
-    return profit
+    // can i buy?
+    if (prices[i] < min) {
+      min = prices[i];
+    }
+  }
+
+  // while(i<prices.length){
+  //     if(prices[i]-min>profit){
+  //         profit=prices[i]-min
+  //     }
+  //     if(prices[i]<min){
+  //         min=prices[i]
+  //     }
+  //     i++
+  // }
+  return profit;
 };
