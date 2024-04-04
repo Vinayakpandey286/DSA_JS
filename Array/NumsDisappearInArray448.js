5b3b1674704f5c994773260a5431a82bf1f9e41f
Vinayak Pandey <109685109+Vinayakpandey286@users.noreply.github.com>
2024-04-04 18:48:51 +0530

    no. disapppear in array

--- Array/NumsDisappearInArray448.js
+++ Array/NumsDisappearInArray448.js
@@ -2,20 +2,33 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var findDisappearedNumbers = function(nums) {
-    
+var findDisappearedNumbers = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    let x = Math.abs(nums[i]);
+    let idx = x - 1;
+    if (nums[idx] > 0) {
+      nums[idx] *= -1;
+    }
+  }
+  let result = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) {
+      result.push(i + 1);
+    }
+  }
+  return result;
 
-    // my solutions(Brute force)
+  // my solutions(Brute force)
 
-    // result=Array(nums.length).fill(0)
-    // for(let i=0;i<nums.length;i++){
-    //     result[nums[i]-1]=1
-    // }
-    // let finalarr = []
-    // for(let j=0;j<result.length;j++){
-    //     if(result[j]===0){
-    //         finalarr.push(j+1)
-    //     }
-    // }
-    // return finalarr
+  // result=Array(nums.length).fill(0)
+  // for(let i=0;i<nums.length;i++){
+  //     result[nums[i]-1]=1
+  // }
+  // let finalarr = []
+  // for(let j=0;j<result.length;j++){
+  //     if(result[j]===0){
+  //         finalarr.push(j+1)
+  //     }
+  // }
+  // return finalarr
 };
