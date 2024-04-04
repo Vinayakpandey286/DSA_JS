56dc00cf39d1fa4aedd7c051619307877de652a9
Vinayak Pandey <109685109+Vinayakpandey286@users.noreply.github.com>
2024-04-04 18:47:42 +0530

    no. disapppear in array

---
+++ Array/NumsDisappearInArray448.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+    
+
+    // my solutions(Brute force)
+
+    // result=Array(nums.length).fill(0)
+    // for(let i=0;i<nums.length;i++){
+    //     result[nums[i]-1]=1
+    // }
+    // let finalarr = []
+    // for(let j=0;j<result.length;j++){
+    //     if(result[j]===0){
+    //         finalarr.push(j+1)
+    //     }
+    // }
+    // return finalarr
+};
