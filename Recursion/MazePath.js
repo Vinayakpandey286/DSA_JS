a6640bff76f3fd28ea1124623e9ee78dc4fee0cb
Vinayak pandey <Vinayakpandey286@gmail.com>
2024-04-30 14:37:23 +0530

    recursion:mazePath

---
+++ Recursion/MazePath.js
@@ -0,0 +1,30 @@
+function Get_Mazepath(sr, sc, dr, dc) {
+  if (sr == dr && sc == dc) {
+    temp = [];
+    temp.push("");
+    return temp;
+  }
+
+  let path_H = [];
+  let path_V = [];
+  if (sc < dc) {
+    path_H = Get_Mazepath(sr, sc + 1, dr, dc);
+  }
+  if (sr < dr) {
+    path_V = Get_Mazepath(sr + 1, sc, dr, dc);
+  }
+
+  const path = [];
+
+  for (let x of path_H) {
+    path.push("h" + x);
+  }
+  for (let y of path_V) {
+    path.push("v" + y);
+  }
+
+  return path;
+}
+
+let path = Get_Mazepath(1, 1, 3, 3);
+console.log(path.length);
