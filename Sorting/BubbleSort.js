40b8b133388b67415dda649385e7422513a76592
Vinayak pandey <Vinayakpandey286@gmail.com>
2024-09-10 11:18:01 +0530

    ...

--- Sorting/BubbleSort.js
+++ Sorting/BubbleSort.js
@@ -1,11 +1,15 @@
 function BubbleSort(arr) {
+    let swapped = false
     let i = arr.length - 1
     while (i > 0) {
         for (let j = 0; j < arr.length; j++) {
             if (arr[j] > arr[j + 1]) {
                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+            } else {
+                swapped = true
             }
         }
+        if (swapped) return arr
         i--
     }
 
@@ -17,4 +21,4 @@ function BubbleSort(arr) {
 // and iterate through to 0 to n-1 and swap the value if j > j+1
 // 4. do i--
 
-console.log(BubbleSort([15, 2, 5, 6, 3, 1, -1]))
+console.log(BubbleSort([2, 5, 6, 1]))
