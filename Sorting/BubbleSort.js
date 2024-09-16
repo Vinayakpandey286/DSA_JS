591f6a63141cdd8aec3d197f571f194cc45a66c5
Vinayak Pandey <109685109+Vinayakpandey286@users.noreply.github.com>
2024-09-16 14:40:54 +0530

    Update BubbleSort.js
    changes in bubble sort

--- Sorting/BubbleSort.js
+++ Sorting/BubbleSort.js
@@ -1,15 +1,14 @@
 function BubbleSort(arr) {
-    let swapped = false
     let i = arr.length - 1
     while (i > 0) {
-        for (let j = 0; j < arr.length; j++) {
+      let swapped = false
+        for (let j = 0; j <= i; j++) {
             if (arr[j] > arr[j + 1]) {
                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
-            } else {
                 swapped = true
             }
         }
-        if (swapped) return arr
+        if (!swapped) return arr
         i--
     }
 
@@ -19,6 +18,7 @@ function BubbleSort(arr) {
 
 // in bubble sort we store the n-1 index 
 // and iterate through to 0 to n-1 and swap the value if j > j+1
+// store swapped var for best case scenario if arr is already sorted
 // 4. do i--
 
 console.log(BubbleSort([2, 5, 6, 1]))
