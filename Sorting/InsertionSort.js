222703fa914b7215e7b98eb29939f34bf20881c2
Vinayak Pandey <109685109+Vinayakpandey286@users.noreply.github.com>
2024-09-17 19:25:20 +0530

    Update InsertionSort.js

--- Sorting/InsertionSort.js
+++ Sorting/InsertionSort.js
@@ -1,1 +1,23 @@
 
+function insertionSort(arr) {
+    
+      for(let i =1; i< arr.length; i++){
+         let current_element = arr[i]
+         let j = i-1
+         while(j>=0 && arr[j]> current_element){
+           arr[j+1] = arr[j]
+           j--
+         }
+         arr[j+1]= current_element
+      }
+      
+      return arr
+    
+  
+}
+
+
+
+const arr = [5,4,3,2,1]
+
+console.log(insertionSort(arr))
